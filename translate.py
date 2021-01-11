99362e818181ccf77356f29a178d78f898a9d07a
jan <jan.van-dick@urbanenergy.de>
2021-01-11 15:15:19 +0100

    Adds choosing language

--- translate.py
+++ translate.py
@@ -1,6 +1,17 @@
+#!/usr/bin/python
+
 import json
 import requests
 import sys
+
+if len(sys.argv) == 1:
+    print("To few arguments: use as following translate.py <sentence_to_translate> optional: <target_lang: DE/EN>")
+    exit()
+
+if len(sys.argv) > 2 and (sys.argv[2] == "DE" or sys.argv[2] == "EN"):
+    target_lang = sys.argv[2]
+else:
+    target_lang = "DE"
 
 payload = {"jsonrpc":"2.0","method": "LMT_handle_jobs",
         "params":{"jobs":
@@ -8,7 +19,7 @@ payload = {"jsonrpc":"2.0","method": "LMT_handle_jobs",
             "raw_en_context_before":[],"raw_en_context_after":[],
             "preferred_num_beams":4,"quality":"fast"}],
             "lang":{"user_preferred_langs":["DE","EN"],"source_lang_user_selected":"auto",
-                "target_lang":"DE"},
+                "target_lang":target_lang},
             "priority":-1,"commonJobParams":{},"timestamp":1610369694627},
         "id":60490012}
 
