8eaf5bcd927152983adb18d756b8e959c05a1ac3
jan <jan.van-dick@urbanenergy.de>
2021-01-11 14:54:25 +0100

    initial skrip

---
+++ translate.py
@@ -0,0 +1,22 @@
+import json
+import requests
+import sys
+
+payload = {"jsonrpc":"2.0","method": "LMT_handle_jobs",
+        "params":{"jobs":
+            [{"kind":"default","raw_en_sentence":sys.argv[1],
+            "raw_en_context_before":[],"raw_en_context_after":[],
+            "preferred_num_beams":4,"quality":"fast"}],
+            "lang":{"user_preferred_langs":["DE","EN"],"source_lang_user_selected":"auto",
+                "target_lang":"DE"},
+            "priority":-1,"commonJobParams":{},"timestamp":1610369694627},
+        "id":60490012}
+
+headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
+r = requests.post("https://www2.deepl.com/jsonrpc", data=json.dumps(payload), headers=headers)
+resp = json.loads(r.text)
+translations = []
+for xs in resp["result"]["translations"]:
+    for x in xs["beams"]:
+        translations.append(x["postprocessed_sentence"])
+print(translations)
